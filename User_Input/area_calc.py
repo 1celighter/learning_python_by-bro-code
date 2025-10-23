da9756130ccbf709f4dab53d627337de6e4708ae
1celighter <pruzytyletbsa@gmail.com>
2025-10-24 01:28:46 +0300

    added 2nd exercise

---
+++ User_Input/area_calc.py
@@ -0,0 +1,7 @@
+
+lenght = float(input("Enter the lenght of a rentangle: "))
+width = float(input("Enter the width of a rentangle: "))
+
+area = lenght * width 
+
+print(f"The area is: {area} cm^2")
