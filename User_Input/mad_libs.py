8215f4b8b3399d18c7b00c5774540a42e45d62a5
1celighter <pruzytyletbsa@gmail.com>
2025-10-24 01:20:52 +0300

    added first exercise

---
+++ User_Input/mad_libs.py
@@ -0,0 +1,13 @@
+
+adjective1 = input("Enter your adjective:")
+noun = input("Enter your noun:")
+adjective2 = input("Enter your  adjective:")
+verb = input("enter ur verb:")
+adjective3 = input("enter your adjective:")
+
+
+print(f"Today i went to a {adjective1} zoo.")
+print(f"in an exhibit, i saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"i was {adjective3}")
+
