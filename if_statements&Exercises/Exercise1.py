74c8fc790adad285da43477c43af371420add930
1celighter <pruzytyletbsa@gmail.com>
2025-10-26 01:25:38 +0300

    added 1st exercise

---
+++ if_statements&Exercises/Exercise1.py
@@ -0,0 +1,6 @@
+response = input("would you like food? (Y/N): ")
+
+if response == "Y":
+    print("good we will take some food for you!")
+else:
+    print("okay if u dont want some food we will not order food for you")
