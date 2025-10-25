358ae9bd20f698e1d309d69962300beca27a30a7
1celighter <pruzytyletbsa@gmail.com>
2025-10-26 01:42:18 +0300

    added to work with booleans and statements

---
+++ if_statements&Exercises/Boolean.py
@@ -0,0 +1,8 @@
+# we can also use only boolean for if else 
+
+for_sale = False # if we repleace to true we will get if answer
+
+if for_sale:
+    print("This item for sale!")
+else:
+    print("This item is NOT for sale!")
