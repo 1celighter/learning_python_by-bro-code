1ea236c5e098d0fede223dc89d99455a122ff14b
1celighter <pruzytyletbsa@gmail.com>
2025-10-31 00:29:44 +0300

    added 2 exercises

---
+++ while_loops&exercise/exercise2.py
@@ -0,0 +1,9 @@
+
+food = input("Enter a food you like (q to quit): ")
+
+while not food == "q":
+    print(f"you like {food}")
+    food = input("Enter another favorite food (q to quit): ")
+
+print("bye")
+
